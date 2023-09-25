ba92f69e38bcffca2b0c839d1b510949097f0488
Allencs16 <brenoallencs@gmail.com>
2023-09-25 19:46:58 -0300

    using dotenv for variables, and response in text for user

--- app.js
+++ app.js
@@ -2,21 +2,30 @@ const express = require('express');
 const app = express();
 const openai = require('openai');
 app.use(express.json()) 
+require('dotenv').config();
 
+const apiKey = process.env.OPENIA_TOKEN;
+const openaiClient = new openai({ apiKey: apiKey });
 
-
-
-// Define a route to interact with GPT-3
 app.get('/gpt3', async (req, res) => {
   try {
     var requestBody = req.body;
     const response = await openaiClient.completions.create({
       model: 'text-davinci-002',
-      prompt: 'Responda se isso tem a ver com o contexto de governanca de TI ou corporativa: ' + requestBody.text,
-      max_tokens: 50,
+      prompt: 'Responda somente com sim ou não se isso tem a ver com o contexto de governanca de TI ou corporativa: ' + requestBody.text,
+      max_tokens: 10,
     });
 
-    res.json({ response: response.choices[0].text });
+    if(response.choices[0].text.replace('\n\n', '').toLowerCase() == 'sim'){
+      const response = await openaiClient.completions.create({
+        model: 'text-davinci-002',
+        prompt: requestBody.text,
+        max_tokens: 200,
+      });
+      res.json({ response: response.choices[0].text });
+    } else {
+      res.json({ response: 'Sua Pergunta não esta alinhada com gorvenança' })
+    }
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'An error occurred' });
